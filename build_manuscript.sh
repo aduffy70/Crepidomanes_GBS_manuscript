655d8d02259c2ddea6520be3df201bf9c7a3432b
Aaron M. Duffy <aduffy70@gmail.com>
2018-09-07 10:32:33 -0600

    Stop using odt format. Switch to doc/docx. Open document standards aren't helpful if no one else can open them.

--- build_manuscript.sh
+++ build_manuscript.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-# Convert markdown manuscript with citations to odt
-pandoc -s -S --reference-odt=./custom-reference.odt --bibliography=citations.bib --csl=ecology.csl -o Builds/Crepidomanes_GBS_manuscript_$(date +%Y%m%d).odt Crepidomanes_GBS_manuscript.md ;
+# Convert markdown manuscript with citations to docx
+pandoc -s -S --reference-doc=./custom-reference.docx --bibliography=citations.bib --csl=ecology.csl -o Builds/Crepidomanes_GBS_manuscript_$(date +%Y%m%d).docx Crepidomanes_GBS_manuscript.md ;
 
-# Convert markdown figures section to odt
-pandoc -s -S --reference-odt=./custom-reference-landscape.odt -o Builds/Crepidomanes_GBS_manuscript_figures_$(date +%Y%m%d).odt Crepidomanes_GBS_manuscript_figures.md ;
+# Convert markdown figures section to docx
+pandoc -s -S --reference-doc=./custom-reference-landscape.docx -o Builds/Crepidomanes_GBS_manuscript_figures_$(date +%Y%m%d).docx Crepidomanes_GBS_manuscript_figures.md ;
 
-# Tables are already in odt format...
+# Tables are already in doc format...
